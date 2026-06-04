de230b4f7a512e016cf25ec1659aec3cd423ef62
Titruc <86261407+Titruc@users.noreply.github.com>
2026-06-04 20:16:24 +0200

    advancement

--- clone.sh
+++ clone.sh
@@ -3,15 +3,9 @@
 
 
 cd /opt/website/titruc
-
 rm -rf titruc-website
-
 git clone https://github.com/Titruc/titruc-website/
-
 cd titruc-website
-
 docker rm -f $(docker ps -q --filter publish=14577) || true
-
 docker build -t webserver-titruc .
-
 docker run -d -p 14577:80 webserver-titruc
